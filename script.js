8b6598108ebe0736c945dd541fbbafa89f7b5903
naziha1986 <>
2021-06-03 12:28:28 +0000

    added JC file

---
+++ script.js
@@ -0,0 +1,37 @@
+
+alert("welcome every one,nice to meet you")
+
+var userName;
+userName = "naziha nawasmeh";
+
+console.log(userName);
+
+document.write("<p style='color:brown;'> this is " + userName + " from js </p>")
+var x = "10";
+var y = 20;
+
+console.log(y == x)
+//y and x are not equal in value (20>10)so the result will be false
+console.log(y === x);
+//y and xare not equal in value and type so the result will be false
+
+console.log(y !== x);
+// show the opposite answer
+
+prompt("enter your lucky number?")
+var luckynumber = 14;
+if (userluckynumber = 14) {
+  alert("meeee too")
+} else {
+  alert("that's a good one")
+}
+
+prompt("enter your year of birth?")
+var year = 1986;
+if (userYear >= 1986) {
+  alert("just like me")
+} else if(year < 1986) {
+  alert("welcome")
+} else{
+  alert("hi")
+}
